94da87475d3fc38736745e63200d52ae5d80d435
Paul Florian Sattlegger <paul.sattlegger@tuwien.ac.at>
2021-10-25 11:58:19 +0200

    Add exercise 1 template

---
+++ code/createindex.py
@@ -0,0 +1,11 @@
+"""
+This file contains your code to create the inverted index. Besides implementing and using the predefined tokenization function (text2tokens), there are no restrictions in how you organize this file.
+"""
+
+
+def text2tokens(text):
+    """
+    :param text: a text string
+    :return: a tokenized string with preprocessing (e.g. stemming, stopword removal, ...) applied
+    """
+    pass
---
+++ retrieval_results/Readme.txt
@@ -0,0 +1,4 @@
+This folder should contain the results of your retrieval runs.
+There should be at least two runs (and therefore two files) in the TREC format:
+- results obtained from using TFIDF
+- results obtained from using BM25
