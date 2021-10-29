bb1732e30b54835f1a233f4f382914f2cd04731e
Paul Florian Sattlegger <paul.sattlegger@tuwien.ac.at>
2021-10-30 00:29:20 +0200

    Integrate tokenization pipeline

--- code/createindex.py
+++ code/createindex.py
@@ -2,17 +2,23 @@
 This file contains your code to create the inverted index. Besides implementing and using the predefined tokenization function (text2tokens), there are no restrictions in how you organize this file.
 """
 import pickle
-from collections import namedtuple
-from nltk.stem import *
 import re
 import string
+from collections import namedtuple
 from html.parser import HTMLParser
 from itertools import islice
 from pathlib import Path
 from time import perf_counter
 from typing import Generator
 
+from nltk.stem import *
+
+APOSTROPHES_REGEX = r'[\'`´]'
+HYPHEN_REGEX = r'(?<=\d)[-–—―](?<=\d)'
+PUNCTUATION_REGEX = r'(?<!\d)[^\w\s](?!\d)'
+
 Article = namedtuple("Article", ["title", "title_id", "bdy"])
+stemmer = PorterStemmer()
 
 
 class ArticleParser(HTMLParser):
@@ -53,11 +59,14 @@ class InvertedIndex:
     def __init__(self):
         self.data: dict[str, list[int]] = {}
 
-    def add(self, term: str, posting: int):
+    def insert(self, term: str, posting: int):
         if term in self.data:
             self.data[term].append(posting)
         else:
             self.data[term] = [posting]
+
+    def search(self, term: str):
+        return self.data.get(term)
 
     def dump(self):
         with open('../index.obj', 'wb') as file:
@@ -94,28 +103,28 @@ def tokenize(text):
     """
     for word in text.split():
         word = remove_apostrophes(word)
-        word = remove_punctation(word)
+        word = remove_punctuation(word)
         if word != "":  # remove "empty" words (if after removal nothing is left)
             yield from remove_hyphen(word)
 
 
 def remove_apostrophes(token):
     # return token.split("'")[0]
-    return re.split('\'|`|´', token)[0]
+    return re.split(APOSTROPHES_REGEX, token)[0]
 
 
 def remove_hyphen(token):
     # remove hyphen only for words but not for numbers
     # (?<!...) is called negative lookbehind assertion https://docs.python.org/3/library/re.html
-    for word in re.split('(?<=\d)[-|–|—|―](?<=\d)', token):
+    for word in re.split(HYPHEN_REGEX, token):
         yield word
 
 
-def remove_punctation(token):
+def remove_punctuation(token):
     # return re.split('\.|,|;', token)[0]
-    # remove punctation only from words not for numbers
+    # remove punctuation only from words not for numbers
     # (?<!...) is called negative lookbehind assertion https://docs.python.org/3/library/re.html
-    token = re.sub(r'(?<!\d)[^\w\s](?!\d)', '', token)
+    token = re.sub(PUNCTUATION_REGEX, '', token)
     return token.strip(string.punctuation)  # to cover (1856–1953)
 
 
@@ -125,7 +134,7 @@ def stem(term):
     :param term: A term string ready for stemming
     :return: the stemmed string
     """
-    return PorterStemmer().stem(term)
+    return stemmer.stem(term)
 
 
 def get_articles(path: str) -> Generator[Article, None, None]:
@@ -138,37 +147,22 @@ def get_articles(path: str) -> Generator[Article, None, None]:
                     yield Article(parser.title, parser.title_id, parser.bdy)
 
 
-def get_terms(article: Article) -> Generator[str, None, None]:
-    for term in article.bdy.split(' '):
-        yield term.strip()
-
-
-def index_article(index: InvertedIndex, article: Article):
-    for term in get_terms(article):
-        index.add(term, article.title_id)
-
-
 def main():
     index = InvertedIndex()
     articles = get_articles('../dataset/wikipedia articles')
 
     # Benchmark
     start = perf_counter()
-    n = 28343
+    n = 283
     for article in islice(articles, n):
-        index_article(index, article)
+        for token in text2tokens(article.bdy):
+            index.insert(token, article.title_id)
     articles_total = 283438
     articles_per_second = n / (perf_counter() - start)
     print(f'{articles_per_second} articles/s')
     print(f'{articles_total / articles_per_second / 60} m total')
+    print(index.search('art'))
     index.dump()
-
-    # TODO: aim creation time ~ 30 minutes
-    index = InvertedIndex()
-    index.add("Brutus", Posting("2", 1))
-    index.add("Caesar", Posting("1", 1))
-    index.add("Caesar", Posting("2", 1))
-    print(index)
 
     """plurals = ['caresses', 'flies', 'dies', 'mules', 'denied', 'died', 'agreed', 'owned', 'humbled', 'sized', 'meeting',
                'stating', 'siezing', 'itemization', 'sensational', 'traditional', 'reference', 'colonizer', 'plotted']
@@ -176,7 +170,7 @@ def main():
     singles = [stem(plural) for plural in plurals]
     print(' '.join(singles))"""
 
-    testinputtext = "Apostrophes: nation's Australia's other's another`s different´s apostrophes" \
+    test_inputtext = "Apostrophes: nation's Australia's other's another`s different´s apostrophes" \
                     "Hyphens: simple-hyphen ndash–hyphen mdash—hyphen horbar―hyphen ISBN 0-679-73392-2  1805–1872 (1856–1953) " \
                     "Infobox Football biography Defender/Sweeper Cabezón'' San Lorenzo de Almagro Chivas UAG Tecos" \
                     " Independiente Elche CF Club América San Lorenzo de Almagro 097 0(7) gold medal s in the nation's " \
@@ -187,13 +181,13 @@ def main():
                     " ((each with roughly 100 men and led by a captain , with associated lieutenant s). Field" \
                     " officers normally included a colonel  (commanding), most frequent corps per army 1  4  2.74 2  divisions per (((.corps)  ( Rifle &amp; Light pages 180-81"
 
-    for token in text2tokens(testinputtext):
+    for token in text2tokens(test_inputtext):
         print(token)
 
     # Test apostrophes
     print("---- apostophes test ----")
-    apostrophesTest = "in the nation's Australia's other's another`s different´s apostrophes"
-    print(remove_apostrophes(apostrophesTest))
+    apostrophes_test = "in the nation's Australia's other's another`s different´s apostrophes"
+    print(remove_apostrophes(apostrophes_test))
 
     # TODO: parse HTML/XML files
     # TODO: save and load inverted index (pickle)
