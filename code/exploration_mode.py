39a3a02df487d34603ee7ac67a472bdb29b7e2e3
Paul Florian Sattlegger <paul.sattlegger@tuwien.ac.at>
2021-10-30 12:47:26 +0200

    Add sample code for InvertedIndex 'API'

--- code/exploration_mode.py
+++ code/exploration_mode.py
@@ -2,3 +2,13 @@
 This file contains your code for the interactive exploration mode where a string can be input by a user and a ranked list of documents is returned.
 Make sure that the user can switch between TF-IDF and BM25 scoring functions.
 """
+
+from createindex import InvertedIndex
+
+if __name__ == '__main__':
+    index = InvertedIndex.load()
+
+    queries = ['fine', 'art', 'england', '2000']
+    postings = index.search(queries[0])
+    article = index.fetch(postings[0])
+    print(article.bdy)
