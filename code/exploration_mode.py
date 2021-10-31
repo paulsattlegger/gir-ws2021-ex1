6a1e7c7d5f9f6615eaab7338fabe9d87ac2d6c27
Paul Florian Sattlegger <paul.sattlegger@tuwien.ac.at>
2021-10-31 13:24:37 +0100

    Update sample code for InvertedIndex 'API'

--- code/exploration_mode.py
+++ code/exploration_mode.py
@@ -4,11 +4,17 @@ Make sure that the user can switch between TF-IDF and BM25 scoring functions.
 """
 
 from createindex import InvertedIndex
+import numpy as np
 
 if __name__ == '__main__':
     index = InvertedIndex.load()
 
     queries = ['fine', 'art', 'england', '2000']
-    postings = index.search(queries[0])
-    article = index.fetch(postings[0])
-    print(article.bdy)
+
+    ar1 = index.search(queries[0])
+    for i in range(1, len(queries)):
+        ar2 = index.search(queries[i])
+        ar1 = np.intersect1d(ar1, ar2)
+
+    article_1 = int(ar1[0])
+    print(index.fetch(article_1).bdy)
